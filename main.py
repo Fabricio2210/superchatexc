972ae5fb99a2331bcbd0f592b596ae6e6c0d5cae
fabriciomedeiros <fabriciomedeiros@remax.com.br>
2023-12-04 19:20:56 -0300

    Added reacts folders

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from wrapper import wrapper
 print(f"running")
 schedule.every().day.at("18:00").do(lambda: wrapper("dsp","superchats"))
+schedule.every().day.at("18:05").do(lambda: wrapper("reacts","superchats"))
 schedule.every().day.at("18:10").do(lambda: wrapper("doody","superchats"))
 try:
     while True:
