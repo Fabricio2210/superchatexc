f552ad5b49d717d814688b790c700a104846e074
Fabricio <fabricio215@hotmail.com>
2024-01-19 21:02:47 -0300

    Added new channel

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ print(f"running!!")
 schedule.every().day.at("18:15").do(lambda: wrapper("dsp","superchats"))
 schedule.every().day.at("18:20").do(lambda: wrapper("reacts","superchats"))
 schedule.every().day.at("18:25").do(lambda: wrapper("doody","superchats"))
+schedule.every().day.at("18:30").do(lambda: wrapper("throwback","superchats"))
 try:
     while True:
         time.sleep(1)
